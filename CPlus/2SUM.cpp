42d20080e7e564b2f94be173271055da2d2cb73d
Miao Jiang <miaojiang1987@gmail.com>
2018-11-01 14:15:01 -0400

    Start with 2SUM and C++
    As headline

---
+++ CPlus/2SUM.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using std;
+#include <map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> m;
+        for (int i=0;i<nums.size();i++){
+            if (m.count(target-nums[i])){
+                return {i, m[target - nums[i]]};
+            }
+            m[nums[i]]=i;
+        }
+        
+        return {};
+    }
+};
