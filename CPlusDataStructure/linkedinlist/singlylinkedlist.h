24f6d5cd2727eb1e1783e90d5b6839a865149c74
Miao Jiang <miaojiang1987@gmail.com>
2018-11-05 14:54:40 -0500

    Setup LinkedinList Project

---
+++ CPlusDataStructure/linkedinlist/singlylinkedlist.h
@@ -0,0 +1,14 @@
+#ifndef SINGLYLINKEDLIST_H_INCLUDED
+#define SINGLYLINKEDLIST_H_INCLUDED
+
+class LLNode{
+public:
+    LLNode(){
+      next=0;
+    }
+    int info;
+    IntSLLNode *next;
+};
+
+
+#endif // SINGLYLINKEDLIST_H_INCLUDED
