943a562e3e10cb94a996365f027253dc49e9dc68
Miao Jiang <miaojiang1987@gmail.com>
2018-11-17 20:18:55 -0500

    Solved Combination Sum!

---
+++ Python/CombinationSum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def combinationSum(self, candidates, target):
+        """
+        :type candidates: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        result=[]
+        if not candidates or len(candidates)==0:
+            return result
+        temp=[]
+        self.dfs(candidates,target,temp,result,0)
+        return result
+    
+    def dfs(self,candidates,target,temp,result,start):
+        
+        if target==0:
+            result.append(temp+[])
+            return
+        
+        if target<0:
+            return
+
+        for i in range(start,len(candidates)):
+            temp.append(candidates[i])
+            self.dfs(candidates,target-candidates[i],temp,result,i)
+            temp.pop()
+        
