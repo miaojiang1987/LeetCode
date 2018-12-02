2d6e723cdf999609c0ca390d2d6b12a54f44f023
Miao Jiang <miaojiang1987@gmail.com>
2018-12-01 22:39:33 -0500

    Solved subsets!

---
+++ Python/subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result=[]
+        if not nums or len(nums)==0:
+            return result
+        temp=[]
+        self.dfs(nums,result,temp,0)
+        return result
+        
+        
+    def dfs(self,nums,result,temp,start):
+        result.append(temp+[])
+        for i in range(start,len(nums)):
+            self.dfs(nums,result,temp+[nums[i]],i+1)
