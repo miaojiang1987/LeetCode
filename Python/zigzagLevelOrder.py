670c55268a21fc224fb50478a4f0fd316fd4a0e9
Miao Jiang <miaojiang1987@gmail.com>
2019-02-09 14:32:29 -0500

    Solved zigzag level order

---
+++ Python/zigzagLevelOrder.py
@@ -0,0 +1,27 @@
+class Solution:
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        
+        res = []
+        queue = collections.deque([root])
+        level = 1
+        while queue:
+            temp = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                temp.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            if level % 2 == 0:
+                temp.reverse()
+            res.append(temp)
+            level += 1
+        return res
+        
