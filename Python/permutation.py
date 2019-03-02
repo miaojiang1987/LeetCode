b7d1e47d1988d85cb695acb87d5e130b8d4f081a
Miao Jiang <miaojiang1987@gmail.com>
2019-03-02 13:46:46 -0500

    Review Permutations

--- Python/permutation.py
+++ Python/permutation.py
@@ -3,25 +3,23 @@ class Solution:
         result=[]
         if not nums or len(nums)==0:
             return result
-        item=[]
-        visit=[0 for i in range(len(nums))]
-        self.dfs(nums,result,item,visit)        
+        
+        visited=[0 for i in range(len(nums))]
+        temp=[]
+        
+        self.dfs(nums,temp,result,visited)
+        
         return result
-        
-        
-        
     
-    def dfs(self,nums,result,item,visit):
-        
-        if len(item)==len(nums):
-            result.append(item+[])
-            return
+    def dfs(self,nums,temp,result,visited):
+        if len(temp)==len(nums):
+            result.append(temp+[])
+            return 
         
         for i in range(len(nums)):
-            if visit[i]: continue
-            num=nums[i]
-            item.append(nums[i])
-            visit[i]=1
-            self.dfs(nums,result,item,visit)
-            visit[i]=0
-            item.pop()
+            if visited[i]: continue
+            temp.append(nums[i])
+            visited[i]=1
+            self.dfs(nums,temp,result,visited)
+            temp.pop()
+            visited[i]=0
