814f3b4f40a90884829beb5bdb2236b2c7c5c450
Miao Jiang <miaojiang1987@gmail.com>
2019-03-03 01:13:34 -0500

    Review subsets

--- Python/subsets.py
+++ Python/subsets.py
@@ -1,18 +1,19 @@
-class Solution:
+class Solution(object):
     def subsets(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        result=[]
         if not nums or len(nums)==0:
-            return result
+            return []
+        
+        result=[]
         temp=[]
         self.dfs(nums,result,temp,0)
         return result
-        
-        
+    
     def dfs(self,nums,result,temp,start):
         result.append(temp+[])
+        
         for i in range(start,len(nums)):
             self.dfs(nums,result,temp+[nums[i]],i+1)
