8c04e5ccf353b6e18b2537a6c4438e69e9e96b87
Miao Jiang <miaojiang1987@gmail.com>
2019-03-03 15:03:35 -0500

    review Same Tree python!

--- Python/isSameTree.py
+++ Python/isSameTree.py
@@ -21,5 +21,4 @@ class Solution(object):
         if p.val!=q.val:
             return False
         
-        else:
-            return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
