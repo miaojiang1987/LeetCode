a04b8b86d88b8cbc33e104374a9fbb2fd42d0389
Miao Jiang <miaojiang1987@gmail.com>
2019-03-04 14:42:08 -0500

    edit distance solved!

---
+++ Python/editDistance.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        m=len(word1)
+        n=len(word2)
+        
+        dp = [[0 for x in range(n+1)] for x in range(m+1)]
+        
+        for i in range(m+1):
+            for j in range(n+1):
+                if i==0:
+                    dp[i][j]=j
+                
+                elif j==0:
+                    dp[i][j]=i
+                
+                elif word1[i-1]==word2[j-1]:
+                    dp[i][j]=dp[i-1][j-1]
+                
+                else:
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+        
+        return dp[m][n]
