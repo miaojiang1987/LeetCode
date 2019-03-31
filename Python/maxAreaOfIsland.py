d530979c6e1f69515b62f068c254cc35ab710b82
Miao Jiang <miaojiang1987@gmail.com>
2019-03-30 22:17:58 -0400

    Review 656

--- Python/maxAreaOfIsland.py
+++ Python/maxAreaOfIsland.py
@@ -7,28 +7,29 @@ class Solution(object):
         if not grid or len(grid)==0:
             return 0
         row=len(grid)
-        columns=len(grid[0])
+        col=len(grid[0])
         visited=set()
-        queue=[]
         
+        queue=[]
         max_area=0
-        #queue.append((0,0))
         
         for i in range(row):
-            for j in range(columns):
+            for j in range(col):
                 if (i,j) in visited: continue
                 if grid[i][j]==1:
                     queue.append((i,j))
-                    current_area=1
+                    count=1
                     visited.add((i,j))
                     while queue:
                         x,y=queue.pop(0)
-                        for dx,dy in [(0,1),(1,0),(-1,0),(0,-1)]:
+                    
+                        for (dx,dy) in [(0,1),(1,0),(0,-1),(-1,0)]:
                             X=x+dx
                             Y=y+dy
-                            if X>=0 and Y>=0 and Y<columns and X<row and grid[X][Y]==1 and (X,Y) not in visited:
+                            if X>=0 and Y>=0 and X<row and Y<col and (X,Y) not in visited and grid[X][Y]==1:
+                                count+=1
                                 visited.add((X,Y))
                                 queue.append((X,Y))
-                                current_area+=1
-                    max_area=max(current_area,max_area)
+                    max_area=max(max_area,count)
+            
         return max_area
