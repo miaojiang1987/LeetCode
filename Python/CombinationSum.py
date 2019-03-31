cec2c485407d162520c5294fa9d6445094302f65
Miao Jiang <miaojiang1987@gmail.com>
2019-03-31 19:26:34 -0400

    Review Combination Sum

--- Python/CombinationSum.py
+++ Python/CombinationSum.py
@@ -5,24 +5,24 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        
         result=[]
-        if not candidates or len(candidates)==0:
-            return result
+        if not candidates or len(candidates)==0: return result
+        
         temp=[]
         self.dfs(candidates,target,temp,result,0)
         return result
     
+    
     def dfs(self,candidates,target,temp,result,start):
-        
         if target==0:
             result.append(temp+[])
             return
         
         if target<0:
             return
-
+        
         for i in range(start,len(candidates)):
             temp.append(candidates[i])
             self.dfs(candidates,target-candidates[i],temp,result,i)
             temp.pop()
-        
