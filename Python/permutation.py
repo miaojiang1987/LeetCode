5949ab2cd56b0fffd4151f0c95796039485799bf
Miao Jiang <miaojiang1987@gmail.com>
2019-04-01 12:16:02 -0400

    Review permutation

--- Python/permutation.py
+++ Python/permutation.py
@@ -1,25 +1,26 @@
 class Solution:
     def permute(self, nums):
         result=[]
+        
         if not nums or len(nums)==0:
-            return result
+            return 0
         
-        visited=[0 for i in range(len(nums))]
         temp=[]
+        visited=[0 for i in range(len(nums))]
         
-        self.dfs(nums,temp,result,visited)
+        self.dfs(nums,result,temp,visited)
         
         return result
     
-    def dfs(self,nums,temp,result,visited):
+    def dfs(self,nums,result,temp,visited):
         if len(temp)==len(nums):
             result.append(temp+[])
-            return 
+            return
         
         for i in range(len(nums)):
             if visited[i]: continue
             temp.append(nums[i])
             visited[i]=1
-            self.dfs(nums,temp,result,visited)
+            self.dfs(nums,result,temp,visited)
             temp.pop()
             visited[i]=0
