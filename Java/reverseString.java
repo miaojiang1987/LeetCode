981474bdf3b5a28cd2919ed2ffc638160db81afd
Miao Jiang <miaojiang1987@gmail.com>
2019-07-01 20:05:01 -0700

    Create reverseString.java
    Solved it by Java

---
+++ Java/reverseString.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+         if (s==null |s.length==0){
+             return;
+         }
+        
+        int i=0;
+        int j=s.length-1;
+        
+       
+        while (i<=j){
+            char tmp=s[i];
+            s[i]=s[j];
+            s[j]=tmp;
+            i++;
+            j--;
+        }
+    }
+}
