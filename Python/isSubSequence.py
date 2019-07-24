5fb6b5ba5da12b5f9a73afb4f066ff91463d71a1
Miao Jiang <miaojiang1987@gmail.com>
2019-07-24 15:48:53 -0700

    solved isSubSequence

---
+++ Python/isSubSequence.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        i=0
+        j=0
+        while i<len(s) and j<len(t):
+            if s[i] == t[j]:
+                i=i+1
+                j=j+1
+            else:
+                j=j+1
+        if i==len(s):
+            return True
+        else:
+            return False
