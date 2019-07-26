7c16239d8d292f57a7361d265f2504c542f8901a
Miao Jiang <miaojiang1987@gmail.com>
2019-07-26 10:29:00 -0700

    Solved 750!

---
+++ Java/countCornerRectangles.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countCornerRectangles(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        count=collections.Counter()
+        ans=0
+        
+        for row in grid:
+            for c1,v1 in enumerate(row):
+                if v1:
+                    for c2 in range(c1+1,len(row)):
+                        if row[c2]:
+                            ans+=count[c1,c2]
+                            count[c1,c2]+=1
+        
+        return ans
