d434bdbb663582dec69c37d1164ae4c902277c9e
Miao Jiang <miaojiang1987@gmail.com>
2019-07-28 16:04:21 -0400

    Update the DFS algorithm

--- Python/1059.py
+++ Python/1059.py
@@ -1,38 +1,21 @@
-class Solution(object):
-    def leadsToDestination(self, n, edges, source, destination):
-        # corner case when source == destination and no edges
-        if source == destination and len(edges) == 0:
-            return True
-        # record the adjacent nodes for each node
-        connections = []
-        for i in range(n):
-            connections.append([])
-        for src, desc in edges:
-            connections[src].append(desc)
-        # when destination has adjacent nodes, it must fail
-        if len(connections[destination]) > 0:
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        g = {}
+        for a,b in edges:
+            g.setdefault(a,[]).append(b)
+        if destination in g:
             return False
-        # for each adjacent nodes from source, dfs
-        count = 0
-        for child in connections[source]:
-            # cache visited nodes for cycle detection
-            hs = set()
-			# dfs
-            stack = [child]
-            while len(stack) > 0:
-                pop = stack.pop()
-                # cycle detection
-                if pop != destination and pop in hs:
+
+        def dfs(root,path):            
+            if root not in g:
+                return root == destination                
+
+            path.add(root)
+            for n in g[root]:
+                if n in path or not dfs(n,path):
                     return False
-                hs.add(pop)
-                # when we reach to a leaf, check if it is our destination
-                if len(connections[pop]) == 0:
-                    if pop == destination:
-                        count += 1
-                    else:
-                        return False
-                # traverse
-                for node in connections[pop]:
-                    stack.append(node)
-		# count of paths must be > 0
-        return count > 0
+            path.remove(root)   
+            return True
+
+
+        return dfs(source,set())
