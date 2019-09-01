992f65707b6c3380ffe92bbdf5ebcdf243158f03
Miao Jiang <miaojiang1987@gmail.com>
2019-09-01 13:03:03 -0400

    Solved Leetcode 76 with C++

---
+++ CPlus/minWinSubstring.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        string res = "";
+        unordered_map<char, int> letterCnt;
+        int left = 0, cnt = 0, minLen = INT_MAX;
+        for (char c : t) ++letterCnt[c];
+        for (int i = 0; i < s.size(); ++i) {
+            if (--letterCnt[s[i]] >= 0) ++cnt;
+            while (cnt == t.size()) {
+                if (minLen > i - left + 1) {
+                    minLen = i - left + 1;
+                    res = s.substr(left, minLen);
+                }
+                if (++letterCnt[s[left]] > 0) --cnt;
+                ++left;
+            }
+        }
+        return res;
+    }
+};
