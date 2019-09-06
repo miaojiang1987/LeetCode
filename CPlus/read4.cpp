50ce35f127c530d0001e2e8f9c3a81d521b4f408
Miao Jiang <miaojiang1987@gmail.com>
2019-09-06 10:26:28 -0400

    Solved 157

---
+++ CPlus/read4.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+    int read(char *buf, int n) {
+        int res = 0;
+        for (int i = 0; i <= n / 4; ++i) {
+            int cur = read4(buf + res);
+            if (cur == 0) break;
+            res += cur;
+        }
+        return min(res, n);
+    }
+};
