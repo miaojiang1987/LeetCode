b95d5e3844be094170c58f6361950e12267bc942
Miao Jiang <miaojiang1987@gmail.com>
2019-09-07 17:11:49 -0400

    Review this problem!

--- Python/isAlienSorted.py
+++ Python/isAlienSorted.py
@@ -5,22 +5,24 @@ class Solution(object):
         :type order: str
         :rtype: bool
         """
-        if not words:
-            return False
         order_index={c:i for i,c in enumerate(order)}
         
-        if len(words)==1: return True
         for i in range(len(words)-1):
             word1=words[i]
             word2=words[i+1]
             
-            for k in range(min(len(word1),len(word2))):
+            
+            size=min(len(word1),len(word2))
+            
+            for k in range(size):
                 if word1[k]!=word2[k]:
                     if order_index[word1[k]]>order_index[word2[k]]:
                         return False
                     break
+                
             else:
                 if len(word1)>len(word2):
                     return False
+        
         
         return True
