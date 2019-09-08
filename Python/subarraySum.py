e8fa2addc34e1da269591d3353c7e6fe06f9a85b
Miao Jiang <miaojiang1987@gmail.com>
2019-09-08 00:03:18 -0400

    Solved 560

---
+++ Python/subarraySum.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def subarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result=0
+        presum=0
+        hashmap={0:1}
+        
+        for i in range(len(nums)):
+            presum+=nums[i]
+            if presum-k in hashmap:
+                result+=hashmap[presum-k]
+            if presum not in hashmap:
+                hashmap[presum]=1
+            else:
+                hashmap[presum]+=1
+        
+        
+        return result
