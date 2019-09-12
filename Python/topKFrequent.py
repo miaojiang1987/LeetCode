c8de0986e6946d2c3f27fe2d72f98b9ddd1a5309
Miao Jiang <miaojiang1987@gmail.com>
2019-09-12 14:35:07 -0400

    Solved top k Frequent

---
+++ Python/topKFrequent.py
@@ -0,0 +1,25 @@
+import collections
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        hashmap={}
+        for num in nums:
+            if num not in hashmap:
+                hashmap[num]=1
+            else:
+                hashmap[num]+=1
+        
+        heap=[]
+        for num,count in hashmap.items():
+            heapq.heappush(heap,(count,num))
+            if len(heap)>k:
+                heapq.heappop(heap)
+        
+        result=[item[1] for item in heap]
+        
+        return result
