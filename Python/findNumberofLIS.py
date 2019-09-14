57d00c7edc94861a2f588b4e416916040058493b
Miao Jiang <miaojiang1987@gmail.com>
2019-09-14 13:45:34 -0400

    Solve new problems

---
+++ Python/findNumberofLIS.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findNumberOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        N = len(nums)
+        if N <= 1: return N
+        lengths = [0] * N #lengths[i] = longest ending in nums[i]
+        counts = [1] * N #count[i] = number of longest ending in nums[i]
+
+        for j, num in enumerate(nums):
+            for i in xrange(j):
+                if nums[i] < nums[j]:
+                    if lengths[i] >= lengths[j]:
+                        lengths[j] = 1 + lengths[i]
+                        counts[j] = counts[i]
+                    elif lengths[i] + 1 == lengths[j]:
+                        counts[j] += counts[i]
+
+        longest = max(lengths)
+        return sum(c for i, c in enumerate(counts) if lengths[i] == longest)
--- Python/threeSum.py
+++ Python/threeSum.py
@@ -7,31 +7,27 @@ class Solution(object):
         num.sort()
         result=[]
         N=len(num)
-        
-        for t in range(N-2):
-            if t>0 and num[t]==num[t-1]:
+        for i in range(N-2):
+            if i>0 and num[i]==num[i-1]:
                 continue
             
-            i,j=t+1,N-1
+            j=i+1
+            k=N-1
             
-            while i<j:
-                _sum=num[t]+num[i]+num[j]
+            while j<k:
+                _sum=num[i]+num[j]+num[k]
                 
                 if _sum==0:
-                    result.append([num[t],num[i],num[j]])
-                    i+=1
-                    j-=1
-                    
-                    while i<j and num[i]==num[i-1]:
-                        i+=1
-                    
-                    while i<j and num[j]==num[j+1]:
-                        j-=1
-                
-                elif _sum<0:
-                    i+=1
-                
+                    result.append([num[i],num[j],num[k]])
+                    j+=1
+                    k-=1
+                    while j < k and num[j] == num[j-1]:
+                        j += 1
+                    while j < k and num[k] == num[k+1]:
+                        k -= 1
+                elif _sum < 0:
+                    j += 1
                 else:
-                    j-=1
-            
+                    k -= 1
+                        
         return result
