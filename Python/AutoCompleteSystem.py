9f0e135c49cf5491f9f07ab3a3a0dbbdc3dae9b6
Miao Jiang <miaojiang1987@gmail.com>
2019-09-17 23:53:21 -0400

    Review 642

--- Python/AutoCompleteSystem.py
+++ Python/AutoCompleteSystem.py
@@ -1,72 +1,38 @@
 class TrieNode:
     def __init__(self):
-        self.child = {}
-        self.end = False
-        self.freq = 0
-        self.content = ""
-    
-    def __lt__(self,other):
-        if self.freq<other.freq:
-            return True
-        elif self.freq>other.freq:
-            return False
-        else:
-            return self.content>other.content
+        self.children = dict()
+        self.sentences = set()
 
-        
 class AutocompleteSystem(object):
-    
-    def __init__(self, sentences, times):
-        """
-        :type sentences: List[str]
-        :type times: List[int]
-        """
-        self.root = TrieNode()
-        self.m = collections.defaultdict(int)
-        for ind,s in enumerate(sentences):
-            self._build(s,times[ind])
-        self.acc = ""
 
-        
-    def _build(self,sentence,time):
-        node = self.root
-        for l in sentence:
-            if l not in node.child:
-                node.child[l] = TrieNode()
-            node = node.child[l]
-        node.end = True
-        node.content = sentence
-        self.m[sentence] += time
-        node.freq = self.m[sentence]
-    
-    def _dfs(self,node,h=[]):
-        # print(node.content,"content")
-        if not node.child or node.end:
-            heapq.heappush(h,node)
-            if len(h)>3:
-                heapq.heappop(h)
-        for w in node.child:
-            self._dfs(node.child[w],h)
-        
-    def input(self, c):
-        """
-        :type c: str
-        :rtype: List[str]
-        """
-        if c=="#":
-            self._build(self.acc,1)
-            self.acc = ""
-            return []
-        self.acc += c
-        h = []
-        node = self.root
-        for l in self.acc:
-            if l not in node.child:
-                return []
-            else:
-                node = node.child[l]
-        self._dfs(node,h)
+    def __init__(self, sentences: List[str], times: List[int]):
+        self.buffer = ''
+        self.stimes = collections.defaultdict(int)
+        self.trie = TrieNode()
+        for s, t in zip(sentences, times):
+            self.stimes[s] = t
+            self.addSentence(s)
+        self.tnode = self.trie
+
+    def input(self, c: str) -> List[str]:
         ans = []
-        while h:
-            ans.append(heapq.heappop(h).content)
-        return ans[::-1]
+        if c != '#':
+            self.buffer += c
+            if self.tnode: self.tnode = self.tnode.children.get(c)
+            if self.tnode: ans = sorted(self.tnode.sentences, key=lambda x: (-self.stimes[x], x))[:3]
+        else:
+            self.stimes[self.buffer] += 1
+            self.addSentence(self.buffer)
+            self.buffer = ''
+            self.tnode = self.trie
+        return ans
+
+    def addSentence(self, sentence):
+        node = self.trie
+        for letter in sentence:
+            child = node.children.get(letter)
+            if child is None:
+                child = TrieNode()
+                node.children[letter] = child
+            node = child
+            child.sentences.add(sentence)
