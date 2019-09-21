85390aa99d018905961c25b3b489973d6ff324f0
Miao Jiang <miaojiang1987@gmail.com>
2019-09-21 12:56:33 -0400

    Review 273

--- Python/numberToWords.py
+++ Python/numberToWords.py
@@ -4,57 +4,45 @@ class Solution(object):
         :type num: int
         :rtype: str
         """
-        def one(num):
-            switcher={1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine'}
-            return switcher.get(num)
-        
-        def two_less_20(num):
-            switcher={10:'Ten',11:'Eleven',12:'Twelve',13:'Thirteen',14:'Fourteen',15:'Fifteen',16:'Sixteen',17:'Seventeen',18:'Eighteen',19:'Nineteen'}
-            return switcher.get(num)
-        
-        def get_10(num):
-            switcher={2:'Twenty',3:'Thirty',4:'Forty',5:'Fifty',6:'Sixty',7:'Seventy',8:'Eighty',9:'Ninety'}
-            return switcher.get(num)
-        
-        def get_two(num):
-            if not num:
-                return ''
-            elif num<10:
-                return one(num)
-            elif num>=10 and num<20:
-                return two_less_20(num)
-            else:
-                tenner=num//10
-                rest=num-tenner*10
-                return get_10(tenner)+ ' '+ one(rest) if rest else get_10(tenner)
-        
-        def get_three(num):
-            hundred= num//100
-            rest=num-hundred*100
-            if hundred and rest:
-                return one(hundred)+' Hundred '+get_two(rest)
-            elif not hundred and rest:
-                return get_two(rest)
-            elif hundred and not rest:
-                return one(hundred)+ ' Hundred'
-        
-        billion=num//1000000000
-        million=(num-billion*1000000000)//1000000
-        thousand=(num-billion*1000000000-million*1000000)//1000
-        rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000
-        
         if not num:
             return 'Zero'
-        result=''
-        if billion:
-            result+=get_three(billion)+' Billion'
-        if million:
-            result+=' ' if result else ''
-            result+=get_three(million)+' Million'
-        if thousand:
-            result+=' ' if result else ''
-            result+=get_three(thousand)+' Thousand'
-        if rest:
-            result+=' ' if result else ''
-            result+=get_three(rest)
-        return result
+
+        self.less_20 = {1:'One',2:'Two',3:'Three',4:'Four',5:'Five',6:'Six',7:'Seven',8:'Eight',9:'Nine',
+                            10:'Ten',11:'Eleven',12:'Twelve',13:'Thirteen',14:'Fourteen',15:'Fifteen',16:'Sixteen',
+                            17:'Seventeen',18:'Eighteen',19:'Nineteen'}
+        
+        self.tenth = {2:'Twenty',3:'Thirty',4:'Forty',5:'Fifty',6:'Sixty',7:'Seventy',8:'Eighty',9:'Ninety'}
+        
+        self.thousands = ["","Thousand","Million","Billion"]
+       
+        res = ""
+        for i in range(len(self.thousands)):
+        # 三位为一个单位，如果超过3位数
+            if num % 1000 != 0:
+                res = self.get_three(num%1000) + " " + self.thousands[i] + " " + res
+            num //= 1000
+
+        return res.strip()        
+    
+    def get_two(self, num):
+        if not num:
+            return ''
+        elif num<20:
+            return self.less_20[num]
+        else:
+            ten=num//10
+            rest=num-ten*10
+            if rest:
+                return self.tenth[ten]+ ' ' + self.less_20[rest] 
+            else:
+                return self.tenth[ten]
+        
+    def get_three(self, num):
+        hundred= num//100
+        rest=num-hundred*100
+        if hundred and rest:
+            return self.less_20[hundred]+' Hundred '+self.get_two(rest)
+        elif hundred and not rest:
+            return self.less_20[hundred]+ ' Hundred'            
+        elif not hundred and rest:
+            return self.get_two(rest)
