000524a4bbf3c84fd9f4514c26f6838e2c21364f
Miao Jiang <miaojiang1987@gmail.com>
2019-09-21 00:07:28 -0400

    Update read4

--- Python/read4.py
+++ Python/read4.py
@@ -1,13 +1,22 @@
-class Solution(object):
+class Solution:
     def read(self, buf, n):
         """
         :type buf: Destination buffer (List[str])
         :type n: Number of characters to read (int)
         :rtype: The number of actual characters read (int)
         """
-        c, m, buf4 = 0, 4, [0] * 4
-        while c < n and m == 4:
-            m = read4(buf4)
-            buf[c:m] = buf4[:m]
-            c += m
-        return min(c, n)
+
+        index = 0
+        remain = n
+        while remain > 0:
+            buf4 = [0] * 4
+            size = read4(buf4)
+            if size == 0:
+                return index
+            
+            for i in range(min(size, remain)):
+                buf[index] = buf4[i]
+                index += 1
+                remain -= 1
+                
+        return index
