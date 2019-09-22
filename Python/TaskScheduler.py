045f95e429904243687afc72bc130b93b6c64f8e
Miao Jiang <miaojiang1987@gmail.com>
2019-09-22 00:06:02 -0400

    Review 621

--- Python/TaskScheduler.py
+++ Python/TaskScheduler.py
@@ -6,30 +6,32 @@ class Solution(object):
         :rtype: int
         """
         hashmap={}
+        
         for c in tasks:
             if c not in hashmap:
                 hashmap[c]=1
             else:
                 hashmap[c]+=1
-        heap=[]
-        for task,count in hashmap.items():
-            heapq.heappush(heap,-count)
         
+        pq=[]
+        for item,count in hashmap.items():
+            heapq.heappush(pq,-count)
         result=0
         
-        while heap:
+        while pq:
             stack=[]
             time=0
             for _ in range(n+1):
-                if heap:
-                    count=heapq.heappop(heap)
+                if pq:
+                    count=heapq.heappop(pq)
                     time+=1
                     if count<-1:
                         stack.append(count+1)
-            for item in stack:
-                heapq.heappush(heap,item)
             
-            if heap:
+            for item in stack:
+                heapq.heappush(pq,item)
+            
+            if pq:
                 result+=n+1
             else:
                 result+=time
