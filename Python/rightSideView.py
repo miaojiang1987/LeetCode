6afcf924399396040eaa29b376ecea4469d02ce3
Miao Jiang <miaojiang1987@gmail.com>
2019-09-22 11:12:52 -0400

    Reveiw 199

--- Python/rightSideView.py
+++ Python/rightSideView.py
@@ -1,24 +1,23 @@
+from collections import deque
+
 class Solution(object):
     def rightSideView(self, root):
         """
         :type root: TreeNode
         :rtype: List[int]
         """
-        right_most_value=dict()
-        max_depth=-1
-        queue=collections.deque([(root,0)])
+        if not root:
+            return []
         
+        queue=deque([root])
+        result=[]
         while queue:
-            node,depth=queue.popleft()
-            
-            if node is not None:
-                max_depth=max(max_depth,depth)
-                
-                right_most_value[depth]=node.val
-                
-                queue.append((node.left,depth+1))
-                queue.append((node.right,depth+1))
+            for _ in range(len(queue)):
+                node=queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(node.val)
         
-        
-        return [right_most_value[depth] for depth in range(max_depth+1)]
-          
+        return result
