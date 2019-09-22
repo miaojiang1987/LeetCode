5c3a319cf2afffc2363f1de04f6435cefc370944
Miao Jiang <miaojiang1987@gmail.com>
2019-09-22 17:46:12 -0400

    Solved 124

--- Python/maxPathSum.py
+++ Python/maxPathSum.py
@@ -4,14 +4,18 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        self.result = float('-inf')
-        self.helper(root)
+        self.result=float('-inf')
+        self.dfs(root)
         return self.result
     
-    def helper(self, root):
+    def dfs(self,root):
         if not root:
             return 0
-        left = max(0, self.helper(root.left))
-        right = max(0, self.helper(root.right))
-        self.result = max(self.result, left + right + root.val)
-        return max(left, right) + root.val
+        
+        left_result=max(self.dfs(root.left),0)
+           
+        right_result=max(self.dfs(root.right),0)
+        
+        self.result=max(self.result,root.val+left_result+right_result)
+        
+        return max(left_result,right_result)+root.val
