04209420506d1f4593e664fe8ad80ea158ee86b3
Miao Jiang <miaojiang1987@gmail.com>
2019-09-25 21:53:43 -0400

    solve 91!

--- Python/decodeWay.py
+++ Python/decodeWay.py
@@ -4,17 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        if s=='' or s[0]=='0':
+        if not s or s[0]=='0':
             return 0
-        dp=[1,1]
-        for i in range(2,len(s)+1):
-            if 10 <=int(s[i-2:i]) <=26 and s[i-1]!='0':
-                dp.append(dp[i-1]+dp[i-2])
-            elif int(s[i-2:i])==10 or int(s[i-2:i])==20:
-                dp.append(dp[i-2])
-            elif s[i-1]!='0':
-                dp.append(dp[i-1])
-            else:
-                return 0
+        n=len(s)
+        dp=[0]*(n+1)
+        dp[0]=dp[1]=1
         
-        return dp[len(s)]
+        for i in range(2,n+1):
+            
+            if '1'<=s[i-1]<='9':
+                dp[i]=dp[i-1]
+            
+            if 10<=int(s[i-2]+s[i-1])<=26:
+                dp[i]+=dp[i-2]
+        
+        
+        
+        return dp[n]
