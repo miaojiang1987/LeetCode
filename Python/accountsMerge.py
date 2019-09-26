2ede5f65c143af0c6f43da675ab02964d6292145
Miao Jiang <miaojiang1987@gmail.com>
2019-09-26 00:37:58 -0400

    Review 721

--- Python/accountsMerge.py
+++ Python/accountsMerge.py
@@ -10,25 +10,27 @@ class Solution(object):
         for account in accounts:
             name=account[0]
             for email in account[1:]:
+                em_to_name[email]=name
                 graph[account[1]].add(email)
                 graph[email].add(account[1])
-                em_to_name[email]=name
         
         visited=set()
         result=[]
         
         for email in graph:
             if email not in visited:
-                visited.add(email)
                 stack=[email]
-                temp=[]
+                visited.add(email)
+                temp=[]        
                 while stack:
                     node=stack.pop()
                     temp.append(node)
-                    for nei in graph[node]:
-                        if nei not in visited:
-                            visited.add(nei)
-                            stack.append(nei)
+                    for neigh in graph[node]:
+                        if neigh not in visited:
+                            stack.append(neigh)
+                            visited.add(neigh)
+            
                 result.append([em_to_name[email]]+sorted(temp))
+        
         
         return result
