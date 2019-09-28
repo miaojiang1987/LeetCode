a1bc608759e5d0f229bc682531477f81d5a7bf88
Miao Jiang <miaojiang1987@gmail.com>
2019-09-28 01:05:12 -0400

    Review 304

--- Python/NumMatrix.py
+++ Python/NumMatrix.py
@@ -8,14 +8,15 @@ class NumMatrix(object):
             self.dp=[]
             return
         
-        n_rows, n_cols=len(matrix), len(matrix[0])
+        row,col=len(matrix),len(matrix[0])
         
-        self.dp=[[0 for j in range(n_cols+1)] for i in range(n_rows+1)]
-
-        for i in range(1, n_rows+1):
-            for j in range(1, n_cols+1):
+        self.dp=[[0 for i in range(col+1)] for j in range(row+1)]
+        
+        for i in range(1,row+1):
+            for j in range(1,col+1):
                 self.dp[i][j]=matrix[i-1][j-1]+self.dp[i-1][j]+self.dp[i][j-1]-self.dp[i-1][j-1]
-
+        
+        
     def sumRegion(self, row1, col1, row2, col2):
         """
         :type row1: int
@@ -28,4 +29,3 @@ class NumMatrix(object):
             return 0
         
         return self.dp[row2+1][col2+1]+self.dp[row1][col1]-self.dp[row1][col2+1]-self.dp[row2+1][col1]
-        
