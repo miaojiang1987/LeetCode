5a79b5004f4afdb03439f4fff9405f0eef995217
Miao Jiang <miaojiang1987@gmail.com>
2019-09-28 15:51:13 -0400

    Review 317

--- Python/shortestDistanceToBuildings.py
+++ Python/shortestDistanceToBuildings.py
@@ -4,40 +4,43 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        R = len(grid)
-        if R == 0:
-            return -1 
-        C = len(grid[0])
+        R=len(grid)
+        C=len(grid[0])
+        if not R or not C:
+            return 0
+        visited=set()
+        D=collections.defaultdict(int)
+        D_buildings=collections.defaultdict(int)
         
-        D = collections.defaultdict(int)
-        D_numBuilding = collections.defaultdict(int)
+        number_of_buildings=0
         
-        num_building = 0
-        
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] != 1:
+        for i in range(R):
+            for j in range(C):
+                if grid[i][j]!=1:
                     continue
-                num_building += 1
-                seen = set([(r,c)])
-                my_queue = [((r,c),0)]
-                while my_queue:
-                    curr,level = my_queue.pop(0)
+                number_of_buildings+=1
+                visited=set((i,j))
+                queue=[((i,j),0)]
+                
+                while queue:
+                    cur,level=queue.pop(0)
                     
-                    if level > 0:
-                        D[curr] += level
-                        D_numBuilding[curr] += 1
+                    if level>0:
+                        D[cur]+=level
+                        D_buildings[cur] += 1
                     
-                    curr_x,curr_y = curr
-                    nxt_points = {(curr_x+1,curr_y),(curr_x-1,curr_y),(curr_x,curr_y+1),(curr_x,curr_y-1)}
-                    for nxt_x,nxt_y in nxt_points:
-                        if 0 <= nxt_x < R and 0 <= nxt_y < C and grid[nxt_x][nxt_y] == 0:
-                            if (nxt_x,nxt_y) not in seen:
-                                seen.add((nxt_x,nxt_y))
-                                my_queue.append(((nxt_x,nxt_y),level+1))
-                                
-        result = float('inf')
-        for k in D_numBuilding:
-            if D_numBuilding[k] == num_building:
-                result = min(result,D[k])
-        return result if result != float('inf') else -1
+                    cur_X,cur_Y=cur
+                    
+                    next_points={(cur_X+1,cur_Y),(cur_X,cur_Y+1),(cur_X-1,cur_Y),(cur_X,cur_Y-1)}
+                    
+                    for next_point_x,next_point_y in next_points:
+                        if 0<=next_point_x<R and 0<=next_point_y<C and grid[next_point_x][next_point_y] == 0:
+                            if (next_point_x,next_point_y) not in visited:
+                                visited.add((next_point_x,next_point_y))
+                                queue.append(((next_point_x,next_point_y),level+1))
+        
+        res = float('inf')
+        for k in D_buildings:
+            if D_buildings[k] == number_of_buildings:
+                res = min(res,D[k])
+        return res if res != float('inf') else -1
