7111969fc40b3bac53ba6b481fb2666946105c9e
Miao Jiang <miaojiang1987@gmail.com>
2019-09-29 13:45:17 -0400

    Review 88!

--- Python/mergeSortedArray.py
+++ Python/mergeSortedArray.py
@@ -1,30 +1,23 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
+        Do not return anything, modify nums1 in-place instead.
         """
-        nums1_copy=nums1[:m]
         
-        
-        p1=0
-        p2=0
-        
-        i=0
-        
-        while p1<m and p2<n:
-            if nums1_copy[p1]<nums2[p2]:
-                nums1[i]=nums1_copy[p1]
-                p1+=1
+        index = m+n-1
+        m = m-1
+        n = n-1
+        while m >=0 and n >= 0:
+            if nums1[m] > nums2[n]:
+                nums1[index] = nums1[m]
+                m -= 1
             else:
-                nums1[i]=nums2[p2]
-                p2+=1
-            i+=1
+                nums1[index] = nums2[n]
+                n -= 1
+            index -= 1
+            
+        while n >= 0:
+            nums1[index] = nums2[n]
+            n -= 1
+            index -= 1
         
-        if p1<m:
-            nums1[i:m+n]=nums1_copy[p1:]
-        else:
-            nums1[i:m+n]=nums2[p2:]
