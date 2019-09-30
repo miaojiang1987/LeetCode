7e79ceaefd41ab6aef4ebb2af6ca12e1caec3df8
Miao Jiang <miaojiang1987@gmail.com>
2019-09-29 20:54:57 -0400

    Update common stupid leetcode

--- Python/lowestCommonAncestor.py
+++ Python/lowestCommonAncestor.py
@@ -6,13 +6,16 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
+        
         if not root:
             return None
         
-        if root==p or root==q:
+        if root==q or root==p:
             return root
         
+        
         left=self.lowestCommonAncestor(root.left,p,q)
+        
         right=self.lowestCommonAncestor(root.right,p,q)
         
         if left and right:
