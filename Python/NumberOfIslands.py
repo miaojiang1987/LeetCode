32e2627d24cb75fc42ce4112309910e991136011
Miao Jiang <miaojiang1987@gmail.com>
2019-09-30 22:43:10 -0400

    Review 200

--- Python/NumberOfIslands.py
+++ Python/NumberOfIslands.py
@@ -4,29 +4,31 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
+        if not grid or not grid[0]:
             return 0
+        result=0
         row=len(grid)
         col=len(grid[0])
-        
-        number=0
         visited=set()
-        queue=[]
+        queue=collections.deque()
         
         for i in range(row):
             for j in range(col):
                 if (i,j) in visited: continue
+                
                 if grid[i][j]=='1':
-                    number+=1
+                    result+=1
                     queue.append((i,j))
                     visited.add((i,j))
+                    
                     while queue:
-                        x,y=queue.pop(0)
-                        for (dx,dy) in [(0,1),(1,0),(-1,0),(0,-1)]:
+                        x,y=queue.popleft()
+                        for dx,dy in [(1,0),(0,1),(-1,0),(0,-1)]:
                             X=x+dx
                             Y=y+dy
-                            if X>=0 and X<row and Y>=0 and Y<col and grid[X][Y]=='1' and (X,Y) not in visited:
+                            if X>=0 and Y>=0 and X<row and Y<col and (X,Y) not in visited and grid[X][Y]=='1':
                                 queue.append((X,Y))
                                 visited.add((X,Y))
         
-        return number
+        
+        return result
