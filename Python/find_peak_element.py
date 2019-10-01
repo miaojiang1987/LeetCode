aff2c8f21376c358903e2cf46b2a012e4849548a
Miao Jiang <miaojiang1987@gmail.com>
2019-09-30 21:20:12 -0400

    solved 162!

---
+++ Python/find_peak_element.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return None
+        
+        l,r=0,len(nums)-1
+        
+        while l<r:
+            mid=l+(r-l)//2
+            if nums[mid]>nums[mid+1]:
+                r=mid
+            else:
+                l=mid+1
+            
+        
+        return l
