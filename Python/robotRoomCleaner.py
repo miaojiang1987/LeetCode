dc603cb61f71a8bc1d64a52390cd6a5b12d06bec
Miao Jiang <miaojiang1987@gmail.com>
2019-10-03 13:17:40 -0400

    review 489!

--- Python/robotRoomCleaner.py
+++ Python/robotRoomCleaner.py
@@ -1,31 +1,32 @@
-class Solution(object):
+class Solution:
     def cleanRoom(self, robot):
         """
         :type robot: Robot
         :rtype: None
         """
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        visited=set()
-        self.dfs(0,0,robot,0,directions,visited)
         
-    def go_back(self,robot):
-        robot.turnRight()
-        robot.turnRight()
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)] # 方向要顺时针或逆时针
+        visited = set()
+        self.dfs(0, 0, robot, 0, visited, directions)        
+ 
+    def goBack(self, robot):
+        robot.turnLeft()
+        robot.turnLeft()
         robot.move()
-        robot.turnLeft()
-        robot.turnLeft()
-        
-        
-    def dfs(self,i,j,robot,start,directions,visited):
-        if (i,j) in visited:
+        robot.turnRight()
+        robot.turnRight()
+ 
+    def dfs(self, i, j, robot, start, visited, directions):
+        print(i,j)
+        if (i, j) in visited:
             return
-        visited.add((i,j))
+        visited.add((i, j))
         robot.clean()
-        
+
         for _ in range(len(directions)):
             if robot.move():
-                self.dfs(i+directions[start][0],j+directions[start][1],robot,start,directions,visited)
-                self.go_back(robot)
-        
-            robot.turnRight()
-            start=(start+1)%(len(directions))
+                self.dfs(i+directions[start][0], j+directions[start][1], robot, start, visited, directions)
+                self.goBack(robot)
+            robot.turnLeft()
+            start = (start+1) % len(directions)
+         
