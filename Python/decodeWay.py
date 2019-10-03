fe5a4639372ce43568933b016a79fe1c7a2c1f46
Miao Jiang <miaojiang1987@gmail.com>
2019-10-02 23:11:00 -0400

    Review 91

--- Python/decodeWay.py
+++ Python/decodeWay.py
@@ -6,12 +6,13 @@ class Solution(object):
         """
         if not s or s[0]=='0':
             return 0
+        
         n=len(s)
         dp=[0]*(n+1)
         dp[0]=dp[1]=1
         
+        
         for i in range(2,n+1):
-            
             if '1'<=s[i-1]<='9':
                 dp[i]=dp[i-1]
             
