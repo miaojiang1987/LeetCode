8c3547a7c2441da5cf77a9decf388b1599e66721
Miao Jiang <miaojiang1987@gmail.com>
2019-10-04 21:12:13 -0400

    Review 25

--- Python/reverseNodesKGroup.py
+++ Python/reverseNodesKGroup.py
@@ -1,43 +1,49 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
         if not head:
             return None
-        dummy = ListNode(-1)
-        dummy.next = head
         
-        prev = dummy
-        cur = head
+        dummy=ListNode(-1)
+        dummy.next=head
+        
+        prev=dummy
+        cur=head
         
         while cur:
-            # lead pointer and follow pointer, fast forward the lead pointer by k
-            fast = slow = cur
+            slow=fast=cur
             for _ in range(k-1):
                 if not fast.next:
                     return dummy.next
-                fast = fast.next
-                
-            # bookmark start of next loop
-            cur = fast.next
-            res = self.reverse(slow, fast)
-            prev.next = res[0]
-            slow.next = cur
-            prev = res[1]
-            
+                fast=fast.next
+            cur=fast.next
+            res=self.reverse(slow,fast)
+            prev.next=res[0]
+            slow.next=cur
+            prev=res[1]
+        
+        
         return dummy.next
         
-    def reverse(self, start, end):
-        """Reverse a list until we get to node given by 'end'."""
-        prev, cur, next = None, start, None
-        while prev != end:
-            next = cur.next
-            cur.next = prev
-            prev = cur
-            cur = next
-        return [end, start]
+              
+    # 1->2->3
+    # 1<-2<-3
+    def reverse(self,start,end):
+        prev,cur,next=None,start,None        
+        while prev!=end:
+            next=cur.next
+            cur.next=prev
+            prev=cur
+            cur=next
+        
+        return [end,start]
