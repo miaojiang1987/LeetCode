b8cb696d0dddcbc1831fbd849f895685efae4fea
Miao Jiang <miaojiang1987@gmail.com>
2019-10-05 11:58:43 -0400

    Review 416!

--- Python/canPartition.py
+++ Python/canPartition.py
@@ -1,26 +1,24 @@
-class Solution:
+class Solution(object):
     def canPartition(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
         """
-    
         if not nums:
+            return 0
+        
+        total=sum(nums)
+        if total%2:
             return False
+        target=total//2
         
-        total_sum = sum(nums)
-        if total_sum % 2 == 1:
-            return False
+        dp=[False]*(target+1)
+        dp[0]=True
         
-        target = total_sum // 2
+        for num in nums:
+            if num>target: return False
+            if num==target: return True
+            for i in range(target,num-1,-1):
+                dp[i]=dp[i-num] or dp[i]
         
-        dp = [False]*(target + 1) #初始化dp，全部设为false
-        dp[0] = True
-        for i in range(len(nums)):
-            for j in range(target, nums[i]-1, -1): #要更新[nums[i], target]之间的值
-                print(dp)
-                print(j)
-                if dp[j-nums[i]] == True:  
-                    dp[j] = True
-                #dp[j] = dp[j-nums[i]] or dp[j]
         return dp[target]
