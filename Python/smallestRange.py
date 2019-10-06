6b378099b0f60641011241bb6497ac23d1bd314d
Miao Jiang <miaojiang1987@gmail.com>
2019-10-06 16:59:13 -0400

    Review 632

--- Python/smallestRange.py
+++ Python/smallestRange.py
@@ -4,28 +4,36 @@ class Solution(object):
         :type nums: List[List[int]]
         :rtype: List[int]
         """
-        heap = []
-        cur_max = float('-inf')
-        for idx, num in enumerate(nums):
-            if num[0] > cur_max:
-                cur_max = num[0]
-            heapq.heappush(heap, (num[0], idx))
-            num.pop(0)
+        _max=float('-inf')
+        _min=float('inf')
+        pq=[]
         
-        res = []   
-        min_range = float('inf')
+        for list_index in range(len(nums)):
+            if nums[list_index][0]>_max:
+                _max=nums[list_index][0]
+            
+            if nums[list_index][0]<_min:
+                _min=nums[list_index][0]
+            
+            heapq.heappush(pq,(nums[list_index][0], list_index))
+            nums[list_index].pop(0)
         
-        while heap:            
-            next_num, ls_idx = heapq.heappop(heap)
-            if cur_max - next_num < min_range or cur_max - next_num == min_range and next_num < res[0]:
-				res = [next_num, cur_max]
-				min_range = cur_max - next_num
-            if nums[ls_idx]:
-				heapq.heappush(heap, (nums[ls_idx][0], ls_idx))
-				if nums[ls_idx][0] > cur_max:
-					cur_max = nums[ls_idx][0]
-				nums[ls_idx].pop(0)
+        min_range=_max-_min
+        res = [_min, _max]
+        
+        while pq:
+            num,index=heapq.heappop(pq)
+            if _max-num<min_range or (_max-num==min_range and num<res[0]):
+                res = [num, _max]
+                min_range = _max - num
+            
+            if nums[index]:
+                heapq.heappush(pq,(nums[index][0], index))
+                if nums[index][0] > _max:
+                    _max = nums[index][0]
+                nums[index].pop(0)
             else:
                 break
+        
         
         return res
