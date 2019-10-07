2aee01a9b591f3d835f72ee2966c2c0535997c6e
Miao Jiang <miaojiang1987@gmail.com>
2019-10-06 23:14:18 -0400

    921 Solved!

---
+++ Python/minAddToMakeValid.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def minAddToMakeValid(self, S):
+        """
+        :type S: str
+        :rtype: int
+        """
+        if not S:
+            return 0
+        result=0
+        left_count=0
+        right_count=0
+        
+        for i in range(len(S)):
+            if S[i]=='(':
+                left_count+=1
+            
+            if S[i]==')':
+                if left_count!=0:
+                    left_count-=1
+                else:
+                    right_count+=1
+        
+        
+        return left_count+right_count
