55b18023e221a7618e7c01147681134e57b23907
Miao Jiang <miaojiang1987@gmail.com>
2019-10-07 17:14:13 -0400

    review 935

--- Python/kinghtDialer.py
+++ Python/kinghtDialer.py
@@ -8,12 +8,15 @@ class Solution(object):
         moves = [[4,6],[6,8],[7,9],[4,8],[3,9,0],[],
                      [1,7,0],[2,6],[1,3],[2,4]]
 
-        dp = [1] * 10
-        for hops in xrange(N-1):
-            dp2 = [0] * 10
-            for node, count in enumerate(dp):
+        dp=[1]*10
+        
+        for hops in range(N-1):
+            dp1=[0]*10
+            for node,count in enumerate(dp):
                 for nei in moves[node]:
-                    dp2[nei] += count
-                    dp2[nei] %= MOD
-            dp = dp2
-        return sum(dp) % MOD
+                    dp1[node]+=dp[nei]
+                    dp1[node]%=MOD
+            
+            dp=dp1
+        
+        return sum(dp)%MOD
