e336a132db7db250685215a62c6ab53983b64bfa
Miao Jiang <miaojiang1987@gmail.com>
2019-10-06 20:44:18 -0400

    Solved 380

---
+++ Python/randomizedSet.py
@@ -0,0 +1,47 @@
+class RandomizedSet(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.hashmap={}
+        self.list=[]
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.hashmap:
+            self.hashmap[val]=len(self.list)
+            self.list.append(val)
+            return True
+        
+        return False
+        
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        if val not in self.hashmap:
+            return False
+        index=self.hashmap[val]
+        node=self.list.pop()
+        
+        if index<len(self.list):
+            self.list[index]=node
+            self.hashmap[node]=index 
+        del self.hashmap[val]
+        return True
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return random.choice(self.list)
