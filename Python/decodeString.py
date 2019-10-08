15170be2982c4ad960ba430b01e4ff89611c7774
Miao Jiang <miaojiang1987@gmail.com>
2019-10-07 22:48:16 -0400

    Review 394

--- Python/decodeString.py
+++ Python/decodeString.py
@@ -1,32 +1,35 @@
 class Solution(object):
     def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
         if not s:
             return ''
         
-        res = []
-        stack = []
+        res=[]
+        stack=[]
         
         for i, x in enumerate(s,0):
             if x.isdigit():
-                if i > 0 and s[i-1].isdigit():
-                    stack[-1][0] = stack[-1][0] * 10 + int(x)
+                if i>0 and s[i-1].isdigit():
+                    stack[-1][0]=stack[-1][0] * 10 + int(x)
                 else:
-                    stack.append([int(x),'']) 
-					
+                    stack.append([int(x),''])
+            
             elif x.isalpha():
                 if not stack:
                     res += x
                 else:
                     stack[-1][1] += x
-					
-            elif x == ']':
-			
-                n, chars = stack.pop()
-				
-		""" non-empty stack means nested []'s, so append the string to the inner []'s string"""
-                if stack:
-                    stack[-1][1] += chars * n
-                else:
-                    res += chars * n  
             
+            elif x==']':
+                n,chars=stack.pop()
+                if stack:
+                    stack[-1][1]+=n*chars
+                else:
+                    res+=n*chars
+            
+        
+        
         return ''.join(res)
