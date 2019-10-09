183bc92e396497380a470429906ccec684dad67f
Miao Jiang <miaojiang1987@gmail.com>
2019-10-09 12:36:43 -0400

    Solved 863

---
+++ Python/allNodesDistKBT.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def distanceK(self, root, target, K):
+        """
+        :type root: TreeNode
+        :type target: TreeNode
+        :type K: int
+        :rtype: List[int]
+        """
+        def dfs(node, par = None):
+            if node:
+                node.par = par
+                dfs(node.left, node)
+                dfs(node.right, node)
+
+        dfs(root)
+
+        queue = collections.deque([(target, 0)])
+        seen = {target}
+        while queue:
+            if queue[0][1] == K:
+                return [node.val for node, d in queue]
+            node, d = queue.popleft()
+            for nei in (node.left, node.right, node.par):
+                if nei and nei not in seen:
+                    seen.add(nei)
+                    queue.append((nei, d+1))
+
+        return []
