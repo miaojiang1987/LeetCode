a1848508396c8d92160a70c2bf51e817973609e4
Miao Jiang <miaojiang1987@gmail.com>
2019-10-10 23:49:31 -0400

    207 Review!

--- Python/courseSchedule.py
+++ Python/courseSchedule.py
@@ -1,33 +1,36 @@
-class Solution:
+class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
+        graph={}
+        indegree={}
         
-        # initialize graph and indegree
-        graph = {}
-        indegree = {}
         for i in range(numCourses):
-            indegree[i] = 0
-            graph[i] = []
+            indegree[i]=0
+            graph[i]=[]
         
-        for i, j in prerequisites:
+        for i,j in prerequisites:
             graph[j].append(i)
-            indegree[i] += 1
-            
-        count = 0
-        queue = collections.deque()
+            indegree[i]+=1
+        
+        count=0
+        queue=collections.deque()
+        
         for course in indegree:
-            if indegree[course] == 0:
+            if indegree[course]==0:
                 queue.append(course)
+        
         while queue:
-            course = queue.popleft()
-            count += 1
-            for neighbor in graph[course]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-                    
-        return count == numCourses
+            node=queue.popleft()
+            count+=1
+            
+            for nei in graph[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    queue.append(nei)
+        
+        
+        return count==numCourses
