eae0ef992a9001d619d4d048104e8582250bc69f
Miao Jiang <miaojiang1987@gmail.com>
2019-10-11 15:08:48 -0400

    Solved 128!

--- Python/longestConsecutive.py
+++ Python/longestConsecutive.py
@@ -1,36 +1,21 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution(object):
-    def longestConsecutive(self, root):
+    def longestConsecutive(self, nums):
         """
-        :type root: TreeNode
+        :type nums: List[int]
         :rtype: int
         """
+        longest_streak = 0
+        num_set = set(nums)
 
-        def longest_path(root):
-            if not root:
-                return 0, 0
-            inc, dec = 1, 1
-            l_inc, l_dec = longest_path(root.left)
-            r_inc, r_dec = longest_path(root.right)
-            if root.left:
-                if root.left.val == root.val + 1:
-                    inc = max(inc, 1 + l_inc)
-                if root.left.val == root.val - 1:
-                    dec = max(dec, 1 + l_dec)
-            if root.right:
-                if root.right.val == root.val + 1:
-                    inc = max(inc, 1 + r_inc)
-                if root.right.val == root.val - 1:
-                    dec = max(dec, 1 + r_dec)
-            res[0] = max(res[0], inc + dec - 1)
-            return (inc, dec)
-        
-        res = [0]
-        longest_path(root)
-        return res[0]
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
