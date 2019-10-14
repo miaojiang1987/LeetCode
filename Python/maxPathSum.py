8a5f0e23f18b669e53f97bba26eecf48870fa0bc
Miao Jiang <miaojiang1987@gmail.com>
2019-10-13 23:51:59 -0400

    Review 124!

--- Python/maxPathSum.py
+++ Python/maxPathSum.py
@@ -1,21 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def maxPathSum(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        self.result=float('-inf')
+        self.max=float('-inf')
         self.dfs(root)
-        return self.result
+        return self.max
     
     def dfs(self,root):
         if not root:
             return 0
-        
         left_result=max(self.dfs(root.left),0)
-           
         right_result=max(self.dfs(root.right),0)
         
-        self.result=max(self.result,root.val+left_result+right_result)
+        self.max=max(self.max,root.val+left_result+right_result)
         
-        return max(left_result,right_result)+root.val
+        return max(root.val+left_result,root.val+right_result)
