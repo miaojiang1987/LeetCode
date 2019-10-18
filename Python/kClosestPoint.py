5bccaefd42311a7b67f3ed23022f3795bb633b54
Miao Jiang <miaojiang1987@gmail.com>
2019-10-17 22:36:59 -0400

    Review by k selection sort

--- Python/kClosestPoint.py
+++ Python/kClosestPoint.py
@@ -5,38 +5,41 @@ class Solution(object):
         :type K: int
         :rtype: List[List[int]]
         """
-        l = 0
-        r = len(points) - 1
-        while l <= r:
-            p = self.partition(points, l, r)
-            #print(p)
-            if p == K:
+        l=0
+        r=len(points)-1
+        while l<=r:
+            p=self.partition(points,l,r)
+            if p==K:
                 return points[:K]
-            elif p > K: # 左侧没完全sort好 
-                r = p-1
+            
+            elif p>K:
+                r=p-1
+            
             else:
-                l = p+1
-                
-        return points[:K]      
-    
+                l=p+1
+        
+        return points[:K]
     
     def partition(self,points,l,r):
         p=l
         pivot=self.distance(points[p])
+        
         while l<=r:
             while l<=r and self.distance(points[l])<=pivot:
                 l+=1
+            
             while l<=r and self.distance(points[r])>=pivot:
                 r-=1
+            
             if l<r:
                 points[l],points[r]=points[r],points[l]
                 l+=1
                 r-=1
         
         points[p],points[r]=points[r],points[p]
-        
         return r
         
+    
     
     
     def distance(self,p):
