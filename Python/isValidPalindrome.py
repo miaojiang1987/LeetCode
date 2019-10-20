ddd0659a14ec1a1dd5f0f9740f41bd42fca1a6f3
Miao Jiang <miaojiang1987@gmail.com>
2019-10-19 23:19:19 -0400

    Review 125!

--- Python/isValidPalindrome.py
+++ Python/isValidPalindrome.py
@@ -6,22 +6,25 @@ class Solution(object):
         """
         if not s:
             return True
+        
         left=0
         right=len(s)-1
         
-        while left<right:
+        while left<=right:
             while left<len(s) and not s[left].isalnum():
                 left+=1
-            while right>=0 and not s[right].isalnum():
+            while right>0 and not s[right].isalnum():
                 right-=1
-            if left >= right:   
+            if left>right:
                 return True
-            left_char=s[left].lower()
-            right_char=s[right].lower()
-            if left_char!=right_char:
+            
+            l=s[left].lower()
+            r=s[right].lower()
+            if l!=r:
                 return False
             
             left+=1
             right-=1
+        
         
         return True
