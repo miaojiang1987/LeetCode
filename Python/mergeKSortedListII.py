2e1daeaf561f12292e18c8ac82ce3044165a0920
Miao Jiang <miaojiang1987@gmail.com>
2019-10-20 18:14:00 -0400

    Solved it with merge two

--- Python/mergeKSortedListII.py
+++ Python/mergeKSortedListII.py
@@ -1,38 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def mergeKLists(self, lists):
         """
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        if not lists:
+        if not lists or len(lists)==0:
             return None
-        left=0
-        right=len(lists)-1
-        while right>0:
-            if left<right:
-                lists[left]=self.mergeTwoSortedList(lists[left],lists[right])
-                left+=1
-                right-=1
-            else:
+        left, right = 0, len(lists) - 1;
+        while right > 0:
+            if left >= right:
                 left = 0
+            else:
+                lists[left] = self.mergeTwoSortedList(lists[left], lists[right])
+                left += 1
+                right -= 1
         return lists[0]
     
     
-    def mergeTwoSortedList(self,l1,l2):
-        dummy=ListNode(0)
+    
+    def mergeTwoSortedList(self,node1,node2):
+        dummy=ListNode(-1)
         cur=dummy
-        while l1 and l2:
-            if l1.val>l2.val:
-                cur.next=l2
-                l2=l2.next
-                cur=cur.next
+        while node1 and node2:
+            if node1.val<node2.val:
+                cur.next=node1
+                node1=node1.next
             else:
-                cur.next=l1
-                l1=l1.next
-                cur=cur.next
-        if l1:
-            cur.next = l1
-        if l2:
-            cur.next = l2
+                cur.next=node2
+                node2=node2.next
+            cur=cur.next
+        
+        if node1:
+            cur.next=node1
+        if node2:
+            cur.next=node2
         
         return dummy.next
