60488e2c9d35d55af4475f239cc081fe28c60856
Miao Jiang <miaojiang1987@gmail.com>
2019-10-20 13:42:46 -0400

    Review 161!

--- Python/isOneEditDistance.py
+++ Python/isOneEditDistance.py
@@ -5,19 +5,23 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        ns,nt=len(s),len(t)
+        ns=len(s)
+        nt=len(t)
         
-        if ns>nt:
+        if ns>nt: 
             return self.isOneEditDistance(t,s)
         
-        if nt-ns>1: return False
+        if nt-ns>2: 
+            return False
         
         for i in range(ns):
             if s[i]!=t[i]:
                 if ns==nt:
                     return s[i+1:]==t[i+1:]
-            
+                
                 else:
                     return s[i:]==t[i+1:]
         
-        return ns+1==nt
+        
+        
+        return nt==ns+1
