61c0b693aa83cb63c0eefdad87fb99157160b33e
Miao Jiang <miaojiang1987@gmail.com>
2019-10-20 13:21:47 -0400

    Review 278!

--- Python/firstBadVersion.py
+++ Python/firstBadVersion.py
@@ -1,3 +1,8 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
 class Solution(object):
     def firstBadVersion(self, n):
         """
@@ -6,12 +11,14 @@ class Solution(object):
         """
         left=1
         right=n
-        while left<right:
+        result=0
+        while left<=right:
             mid=left+(right-left)//2
             if isBadVersion(mid):
-                right=mid
+                result=mid
+                right=mid-1
             else:
                 left=mid+1
         
         
-        return left
+        return result
