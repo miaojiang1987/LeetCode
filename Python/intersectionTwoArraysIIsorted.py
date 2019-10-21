e19d1cf4e1492a9405f866925301a7f081a9887e
Miao Jiang <miaojiang1987@gmail.com>
2019-10-21 14:10:43 -0400

    Solved 350 with sorted array

---
+++ Python/intersectionTwoArraysIIsorted.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if not nums1 or not nums2:
+            return None
+        result=[]
+        
+        nums1.sort()
+        nums2.sort()
+        i=j=0
+        while i<len(nums1) and j<len(nums2):
+            if nums1[i]==nums2[j]:
+                result.append(nums1[i])
+                i+=1
+                j+=1
+            elif nums1[i]<nums2[j]:
+                i+=1
+            else:
+                j+=1
+        
+        
+        return result
