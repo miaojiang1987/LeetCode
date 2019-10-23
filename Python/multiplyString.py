62c5fd6120a79ea4896127e8a2d821d3ca42f9e3
Miao Jiang <miaojiang1987@gmail.com>
2019-10-23 16:55:14 -0400

    Review 43!

--- Python/multiplyString.py
+++ Python/multiplyString.py
@@ -7,23 +7,25 @@ class Solution(object):
         """
         m=len(num1)
         n=len(num2)
+        
         array=[0 for i in range(m+n-1)]
-       
-        for i in range(m-1,-1,-1):
-            for j in range(n-1,-1,-1):
+        
+        for i in range(len(num1)-1,-1,-1):
+            for j in range(len(num2)-1,-1,-1):
                 array[i+j]+=int(num1[i])*int(num2[j])
         
-        
         result=""
+        
         carry=0
         
         for k in range(len(array)-1,-1,-1):
             result=str((array[k]+carry)%10)+result
-            carry=(array[k]+carry)//10
-   
+            carry= (array[k]+carry)//10
+        
         if carry>0:
-            result=str(carry)+result
+            result=(str)(carry)+result
         
         if len(result) > 1 and result[0] == "0":
             return "0"
+        
         return result
