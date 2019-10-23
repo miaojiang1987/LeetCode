871855a32a6e0d02da6bf09fa7def3220aa2d295
Miao Jiang <miaojiang1987@gmail.com>
2019-10-22 21:57:56 -0400

    Review 76.

--- Python/trapRainWater.py
+++ Python/trapRainWater.py
@@ -4,19 +4,27 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        res = 0
-        dp = [0] * len(height)
+        if not height:
+            return 0
         
-        maxLen = 0
-        for i in range(len(height)):
-            dp[i] = maxLen
-            maxLen = max(maxLen, height[i])
+        result=0
+        
+        left_dp=[0 for i in range(len(height))]
+        left_max=height[0]
+        left_dp[0]=0
+        for i in range(1,len(height)):
+            left_dp[i]=left_max
+            left_max=max(left_max,height[i])
+        
+        right_max=height[len(height)-1]
+        for i in range(len(height)-2,-1,-1):
+            maximum=min(right_max,left_dp[i])
+            if maximum>height[i]:
+                result+=maximum-height[i]
             
-        maxLen = 0
-        for i in range(len(height)-1, -1, -1):
-            dp[i] = min(maxLen, dp[i])
-            maxLen = max(maxLen, height[i])
-            if dp[i] > height[i]:
-                res += dp[i] - height[i]
+            right_max=max(right_max,height[i])
+        
+        
+        
+        return result
                 
-        return res
