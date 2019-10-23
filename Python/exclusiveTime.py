d5d2710dc2d1cabfac9ab09d5ebeed32aba8cbcc
Miao Jiang <miaojiang1987@gmail.com>
2019-10-23 14:59:35 -0400

    Review 636

--- Python/exclusiveTime.py
+++ Python/exclusiveTime.py
@@ -5,21 +5,23 @@ class Solution(object):
         :type logs: List[str]
         :rtype: List[int]
         """
-        stack=[]
+        if not logs:
+            return None
         result=[0]*n
+        stack=[]
         prev_time=0
         
         for log in logs:
-            idx, category, time = log.split(':')
+            ID,category,time=log.split(':')
             if category=='start':
                 if stack:
                     result[stack[-1]]+=int(time)-prev_time
-                stack.append(int(idx))
+                stack.append(int(ID))
                 prev_time=int(time)
-            
             else:
-                result[stack[-1]] += int(time) - prev_time + 1
+                result[stack[-1]]+=int(time)-prev_time+1
                 stack.pop()
-                prev_time = int(time) + 1
+                prev_time=int(time)+1
         
-        return result   
+        
+        return result
