06f06c186cf7ef5f55a4f370fa5d2446aa31b8ac
Miao Jiang <miaojiang1987@gmail.com>
2019-10-22 22:54:57 -0400

    Review 139!

--- Python/wordBreak.py
+++ Python/wordBreak.py
@@ -7,12 +7,14 @@ class Solution(object):
         """
         if not s or not wordDict:
             return False
+        
         dp=[False]*(len(s)+1)
         dp[0]=True
-        
         for i in range(1,len(s)+1):
             for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
                     dp[i]=True
+        
+        
         
         return dp[len(s)]
