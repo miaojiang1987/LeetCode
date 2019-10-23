7c8199362b1d5fe94759a989a2aafa04678e0613
Miao Jiang <miaojiang1987@gmail.com>
2019-10-22 23:01:50 -0400

    Solved 236!

--- Python/lowestCommonAncestor.py
+++ Python/lowestCommonAncestor.py
@@ -1,3 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         """
@@ -6,23 +13,20 @@ class Solution(object):
         :type q: TreeNode
         :rtype: TreeNode
         """
-        
         if not root:
             return None
         
-        if root==q or root==p:
+        if root==p or root==q:
             return root
         
+        left_root=self.lowestCommonAncestor(root.left,p,q)
+        right_root=self.lowestCommonAncestor(root.right,p,q)
         
-        left=self.lowestCommonAncestor(root.left,p,q)
-        
-        right=self.lowestCommonAncestor(root.right,p,q)
-        
-        if left and right:
+        if left_root and right_root:
             return root
         
-        if left:
-            return left
+        if left_root:
+            return left_root
         
-        if right:
-            return right
+        if right_root:
+            return right_root
