7cb44467f23253417fb4dd5557169d77386c1927
Miao Jiang <miaojiang1987@gmail.com>
2019-10-23 23:48:50 -0400

    Review 104

--- Python/maxDepth.py
+++ Python/maxDepth.py
@@ -1,17 +1,9 @@
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if not root:
-            return 0
-        
-        if not root.left and not root.right:
-            return 1
-        
-        else:
-            return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Solution(object):
     def maxDepth(self, root):
@@ -22,12 +14,5 @@ class Solution(object):
         if not root:
             return 0
         
-        queue=[root]
-        result=0
-        
-        while queue:
-            result+=1
-            for i in range(len(queue)):
-                node=queue.pop(0)
-                if node.left: queue.append(node.left)
-                if node.right: queue.append(node.right)
+        else:
+            return 1+max(self.maxDepth(root.left),self.maxDepth(root.right))
