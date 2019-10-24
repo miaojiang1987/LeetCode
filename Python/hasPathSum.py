659a4d4d513ef06ae13a571049e9813e0cf088fb
Miao Jiang <miaojiang1987@gmail.com>
2019-10-23 23:54:29 -0400

    Review 112

--- Python/hasPathSum.py
+++ Python/hasPathSum.py
@@ -1,11 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def hasPathSum(self, root, sum):
- 
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        return self.dfs(root,sum)
+    
+    
+    def dfs(self,root,target):
         if not root:
             return False
+        if root.val==target and not root.left and not root.right:  
+            return True
         
-        if not root.left and not root.right:
-            return root.val==sum
+        remain=target-root.val
         
-        else:
-            return self.hasPathSum(root.left,sum-root.val) or self.hasPathSum(root.right,sum-root.val)
+        return self.dfs(root.left,remain) or self.dfs(root.right,remain)
