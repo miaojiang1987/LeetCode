2ebdfd43ca87b8496cce0d9f43399dd683d9a64e
Miao Jiang <miaojiang1987@gmail.com>
2019-10-23 23:36:41 -0400

    Review 987

--- Python/verticalTraversal.py
+++ Python/verticalTraversal.py
@@ -1,25 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution(object):
     def verticalTraversal(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
         """
-        seen = collections.defaultdict(
-                  lambda: collections.defaultdict(list))
-
-        def dfs(node, x=0, y=0):
-            if node:
-                seen[x][y].append(node)
-                dfs(node.left, x-1, y+1)
-                dfs(node.right, x+1, y+1)
-
-        dfs(root)
-        ans = []
-
-        for x in sorted(seen):
-            report = []
-            for y in sorted(seen[x]):
-                report.extend(sorted(node.val for node in seen[x][y]))
-            ans.append(report)
-
-        return ans
+        queue=collections.deque()
+        cols={}
+        row=0
+        queue.append((root,0))
+        
+        while queue:
+            for _ in range(len(queue)):
+                node, col = queue.popleft()
+                if col not in cols:
+                    cols[col] = [[row, node.val]]
+                else:
+                    cols[col].append([row, node.val])
+                if node.left:
+                    queue.append((node.left, col-1))
+                if node.right:
+                    queue.append((node.right, col+1))
+            row+=1
+            
+        sorted_cols = [cols[key] for key in sorted(cols.keys())]
+        print(sorted_cols)
+        sorted_rows=  [sorted(item) for item in sorted_cols]            
+        print(sorted_rows)
+        res = []
+        for item in sorted_rows:
+            res.append([i[1] for i in item])
+        return res
