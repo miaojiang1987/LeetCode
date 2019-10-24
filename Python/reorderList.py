90d58ef20b720db55938c5a9908aeec1a0b71614
Miao Jiang <miaojiang1987@gmail.com>
2019-10-24 13:26:25 -0400

    Review 143

--- Python/reorderList.py
+++ Python/reorderList.py
@@ -1,47 +1,49 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution:
-    def reorderList(self, head: ListNode) -> None:
+class Solution(object):
+    def reorderList(self, head):
         """
-        Do not return anything, modify head in-place instead.
+        :type head: ListNode
+        :rtype: None Do not return anything, modify head in-place instead.
         """
-        
+        #1->2->3->4
         if not head:
             return None
+        
         dummy=ListNode(-1)
         dummy.next=head
-        fast = slow = dummy
+        slow=fast=head
+        
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+            slow=slow.next
+            fast=fast.next.next
         
-        head1 = head
-        head2 = slow.next
-        slow.next = None
+        head2=slow.next
+        slow.next=None
         
-        prev,cur,next=None,head2,None
+        prev,cur,nxt=None,head2,None
+        
         while cur:
-            next=cur.next
+            nxt=cur.next
             cur.next=prev
             prev=cur
-            cur=next
-        head2=prev
+            cur=nxt
+            
         
-        #1,2,3
-        #5,4
-        #1,5,2,4,3
-        cur1, cur2 = head1, head2
+        cur2=head2=prev
+        cur1=head1=dummy.next
+        
         while cur2:
-            temp1=cur1.next
-            temp2=cur2.next
-            cur1.next=cur2
-            cur2.next=temp1
-            cur1=temp1
-            cur2=temp2
-        
+            temp1 = cur1.next
+            temp2 = cur2.next
+            cur1.next = cur2
+            cur2.next = temp1
+            cur1 = temp1
+            cur2 = temp2
+            
         return dummy.next
             
