7c7e3e4f478cb5b9647a1195b8d1946ed524cc7a
Miao Jiang <miaojiang1987@gmail.com>
2019-10-24 13:17:10 -0400

    reverse list
    review 206!

--- Python/reverseList.py
+++ Python/reverseList.py
@@ -1,19 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        if not head:
-            return None
+        prev,cur,nxt=None,head,None
         
-        current=head
-        prev=None
+        #1->2->3
+        #1<-2<-3
         
-        while current:
-            temp=current.next
-            current.next=prev
-            prev=current
-            current=temp
+        while cur!=None:
+            nxt=cur.next
+            cur.next=prev
+            prev=cur
+            cur=nxt
+        
         
         return prev
