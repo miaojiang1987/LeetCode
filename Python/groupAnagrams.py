647d39cb7f4d517521d7a5d7729751ee85f514ea
Miao Jiang <miaojiang1987@gmail.com>
2019-10-24 23:14:49 -0400

    Solved 49!

---
+++ Python/groupAnagrams.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        if not strs:
+            return None
+        
+        result=[]
+        hashmap={}
+        
+        for string in strs:
+            key="".join(sorted(string))
+            if key in hashmap:
+                hashmap[key].append(string)
+            else:
+                hashmap[key]=[string]
+        
+        return hashmap.values()
