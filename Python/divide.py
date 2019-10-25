f5aa7b8c576c730fe927f7f73c03734ba887b1b8
Miao Jiang <miaojiang1987@gmail.com>
2019-10-24 22:48:12 -0400

    Reivew 29

--- Python/divide.py
+++ Python/divide.py
@@ -5,27 +5,30 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        if dividend == -2147483648 and divisor == -1:
-            return 2147483647
+        if dividend==-1*pow(2,31) and divisor==-1:
+            return pow(2,31)-1
+        
         if divisor==1:
             return dividend
-        negative=(dividend<0)^(divisor<0)
-        dvd = abs(dividend)
-        dvs = abs(divisor)
-        res=0
         
+        negative= (dividend<0)^(divisor<0)
+        dvd=abs(dividend)
+        dvs=abs(divisor)
+        res=0
         while dvd>=dvs:
             temp=dvs
             m=1
             
-            while temp<<1<dvd:
+            while temp<<1<=dvd:
                 temp<<=1
                 m<<=1
-            
+                
             dvd-=temp
             res+=m
         
+        
         if not negative:
             return res
+        
         else:
             return ~res+1
