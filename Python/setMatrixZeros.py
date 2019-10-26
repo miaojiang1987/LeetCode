eecad84d89b3d2e9dac40516c4cc19d67fee9c01
Miao Jiang <miaojiang1987@gmail.com>
2019-10-25 23:34:12 -0400

    Review 73.

--- Python/setMatrixZeros.py
+++ Python/setMatrixZeros.py
@@ -5,21 +5,19 @@ class Solution(object):
         :rtype: None Do not return anything, modify matrix in-place instead.
         """
         if not matrix or not matrix[0]:
-            return None
+            return 0
         
-        check=set()
-        m=len(matrix)
-        n=len(matrix[0])
+        zero_set=set()
         
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]==0:
-                    check.add((i,j))
+        for x in range(len(matrix)):
+            for y in range(len(matrix[0])):
+                if matrix[x][y]==0:
+                    zero_set.add((x,y))
         
-        for x,y in check:
-            for i in range(n):
+        for (x,y) in zero_set:
+            for i in range(len(matrix[0])):
                 matrix[x][i]=0
-            for j in range(m):
+            for j in range(len(matrix)):
                 matrix[j][y]=0
         
         
