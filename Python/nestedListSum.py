767b959b9011773febddc36b34b5a0b15ae6054c
Miao Jiang <miaojiang1987@gmail.com>
2019-10-26 22:06:49 -0400

    Review 339!

--- Python/nestedListSum.py
+++ Python/nestedListSum.py
@@ -4,21 +4,23 @@ class Solution(object):
         :type nestedList: List[NestedInteger]
         :rtype: int
         """
+        result=0
         if not nestedList:
             return 0
-        total=0
+        
         queue=[]
+        for nested in nestedList:
+            queue.append((nested,1))
         
-        for item in nestedList:
-            queue.append((item,1))
-        
-        while queue:
-            item,depth=queue.pop(0)
-            if item.isInteger():
-                total+=depth*item.getInteger()
-            else:
-                for new_Item in item.getList():
-                    queue.append((new_Item,depth+1))
+        while queue: 
+            for i in range(len(queue)):
+                node,depth=queue.pop(0)
+                if node.isInteger():
+                    result+=depth*node.getInteger()
+                else:
+                    for lst in node.getList():
+                        queue.append((lst,depth+1))
         
         
-        return total
+        return result
+                
