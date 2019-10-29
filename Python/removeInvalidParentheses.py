9eef4c397a84087b44ffa8d3c235a05e74edc0a5
Miao Jiang <miaojiang1987@gmail.com>
2019-10-28 23:52:38 -0400

    Review 301

--- Python/removeInvalidParentheses.py
+++ Python/removeInvalidParentheses.py
@@ -4,45 +4,45 @@ class Solution(object):
         :type s: str
         :rtype: List[str]
         """
+        if not s:
+            return [""]
         result=[]
-        l=0
-        r=0
+        left=0
+        right=0
         
         for i in range(len(s)):
             if s[i]=='(':
-                l+=1
-            elif s[i]==')':
-                if l>0:
-                    l-=1
+                left+=1
+            elif s[i]==")":
+                if left>0:
+                    left-=1
                 else:
-                    r+=1
+                    right+=1
         
-        self.dfs(result,s,l,r,0)
+        
+        self.dfs(result,s,0,left,right)
         return result
     
     def isValid(self,s):
         count=0
         for c in s:
             if count<0:
-                return False  
+                return False
             if c=='(':
                 count+=1
-            if c==')':
+            elif c==')':
                 count-=1
-            
+        
         return count==0
     
-    def dfs(self,result,s,l,r,start):
+    def dfs(self,result,s,start,l,r):
         if l==0 and r==0 and self.isValid(s):
             result.append(s)
             return
         
         for i in range(start,len(s)):
-            if i!=start and s[i]==s[i-1]:
-                continue
-                
-            if s[i]=='(' and l>0:
-                self.dfs(result,s[:i]+s[i+1:],l-1,r,i)
+            if s[i]=='(' and l>0 and (i==start or s[i] != s[i-1]):
+                self.dfs(result,s[:i]+s[i+1:],i,l-1,r)
             
-            if s[i]==')' and r>0:
-                self.dfs(result,s[:i]+s[i+1:],l,r-1,i)
+            if s[i]==')' and r>0 and (i==start or s[i]!=s[i-1]):
+                self.dfs(result,s[:i]+s[i+1:],i,l,r-1)
