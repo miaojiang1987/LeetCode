e5faa12d06bbe0f73870c0cd5c5a7cb72a6e4cc5
Miao Jiang <miaojiang1987@gmail.com>
2019-10-28 23:59:53 -0400

    Review 236!

--- Python/productExceptSelf.py
+++ Python/productExceptSelf.py
@@ -1,26 +1,15 @@
-class Solution(object):
-    def productExceptSelf(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        if not nums:
-            return None
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        #[1,2,3,4]
-        # From left:[1,1,2,6]
-        # From right:[1,1,2,6] (R=4), [1,1,8,6] (R=12) [1,12,8,6] (R=24)
-        answer=[1 for i in range(len(nums))]
-        answer[0]=1
-
-        for i in range(1,len(nums)):
-            answer[i]=answer[i-1]*nums[i-1]
-      
-        R=1
-        
-        for i in range(len(nums)-1,-1,-1):
-            answer[i]=answer[i]*R
-            R*=nums[i]
-        
-        
-        return answer
+        res = [1]*len(nums)
+        left = 1
+        for i in range(1, len(nums)):
+            left *= nums[i-1]
+            res[i] = left
+            
+        right = 1
+        for i in range(len(nums)-2, -1, -1):
+            right *= nums[i+1]
+            res[i] *= right
+            
+        return res
