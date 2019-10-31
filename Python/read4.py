aebb58773b18ecd07a13c8223835662bdeae56fa
Miao Jiang <miaojiang1987@gmail.com>
2019-10-31 01:15:27 -0400

    review 157!

--- Python/read4.py
+++ Python/read4.py
@@ -1,17 +1,3 @@
-"""
-The read4 API is already defined for you.
-
-    @param buf, a list of characters
-    @return an integer
-    def read4(buf):
-
-# Below is an example of how the read4 API can be called.
-file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
-buf = [' '] * 4 # Create buffer with enough space to store characters
-read4(buf) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
-read4(buf) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
-read4(buf) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
-"""
 class Solution:
     def read(self, buf, n):
         """
@@ -19,19 +5,19 @@ class Solution:
         :type n: Number of characters to read (int)
         :rtype: The number of actual characters read (int)
         """
+        remain=n
         index=0
-        fucking_remain=n
         
-        while fucking_remain>0:
-            buf4=[0]*4
+        while remain>0:
+            buf4=['']*4
             size=read4(buf4)
             if size==0:
                 return index
             
-            for i in range(min(size,fucking_remain)):
-                buf[index]=buf4[i]
-                index+=1
-                fucking_remain-=1
+            for i in range(min(size,remain)):
+                buf[index] = buf4[i]
+                remain -= 1
+                index += 1
         
         
         return index
