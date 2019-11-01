42e15e76c5bc6a4206d7a69c42b3e9eefd7aa262
Miao Jiang <miaojiang1987@gmail.com>
2019-10-31 22:10:28 -0400

    Review Two problems

--- Python/meetingroomII.py
+++ Python/meetingroomII.py
@@ -7,17 +7,18 @@ class Solution(object):
         if not intervals:
             return 0
         
-        result=1
-        intervals.sort()
+        intervals.sort(key=lambda x:x[0])
         
         pq=[]
+        room=1
         heapq.heappush(pq,intervals[0][1])
         
         for i in range(1,len(intervals)):
-            if intervals[i][0]<pq[0]:
-                result+=1
+            if pq[0]>intervals[i][0]:
+                room+=1
             else:
                 heapq.heappop(pq)
+            
             heapq.heappush(pq,intervals[i][1])
         
-        return result
+        return room
--- Python/minWindowSub.py
+++ Python/minWindowSub.py
@@ -9,37 +9,35 @@ class Solution(object):
             return ""
         
         hashmap={}
-        
         for c in t:
-            if c not in hashmap:
-                hashmap[c]=1
-            else:
+            if c in hashmap:
                 hashmap[c]+=1
+            else:
+                hashmap[c]=1
         
-        count=0
-        minLen=len(s)+1
         start=index=0
-        
+        count=0
+        min_length=len(s)+1
         for i in range(len(s)):
             if s[i] in hashmap:
                 hashmap[s[i]]-=1
                 if hashmap[s[i]]>=0:
                     count+=1
             
-            
             while count==len(t):
-                if i-start+1<minLen:
-                    minLen=i-start+1
+                if min_length>i-start+1:
+                    min_length=i-start+1
                     index=start
-                    
+                
                 if s[start] in hashmap:
                     hashmap[s[start]]+=1
                     if hashmap[s[start]]>0:
                         count-=1
+                
                 start+=1
-            
-        if minLen==len(s)+1:
+        
+        
+        if min_length==len(s)+1:
             return ""
         
-        
-        return s[index:index+minLen]       
+        return s[index:index+min_length]
