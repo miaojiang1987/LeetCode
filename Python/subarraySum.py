aca4a8e10b528e2622d69add939b6f0ce68a8f55
Miao Jiang <miaojiang1987@gmail.com>
2019-11-01 10:32:56 -0400

    review 523!

--- Python/subarraySum.py
+++ Python/subarraySum.py
@@ -1,25 +1,24 @@
 class Solution(object):
-    def subarraySum(self, nums, k):
+    def checkSubarraySum(self, nums, k):
         """
         :type nums: List[int]
         :type k: int
-        :rtype: int
+        :rtype: bool
         """
         if not nums:
-            return None
-        
-        hashmap={0:1}
+            return False
+        hashmap={0:-1}
         pre_sum=0
-        result=0
         
         for i in range(len(nums)):
             pre_sum+=nums[i]
-            if pre_sum-k in hashmap:
-                result += hashmap[pre_sum-k]
-            if pre_sum in hashmap:
-                hashmap[pre_sum]+=1
+            mod=pre_sum%k if k!=0 else pre_sum
+            if mod in hashmap:
+                j=hashmap[mod]
+                if i-j>=2:
+                    return True
+            
             else:
-                hashmap[pre_sum]=1
+                hashmap[mod]=i
         
-        
-        return result
+        return False
