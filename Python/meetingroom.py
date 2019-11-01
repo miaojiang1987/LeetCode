fbb9b126013c54e90fe6aee52d60e38cca89727f
Miao Jiang <miaojiang1987@gmail.com>
2019-10-31 21:17:01 -0400

    review 252!

--- Python/meetingroom.py
+++ Python/meetingroom.py
@@ -7,10 +7,11 @@ class Solution(object):
         if not intervals:
             return True
         
-        intervals.sort()
+        intervals.sort(key=lambda x:x[0])
         
-        for i in range(1,len(intervals)):
-            if intervals[i-1][1]>intervals[i][0]:
+        for i in range(len(intervals)-1):
+            if intervals[i+1][0]<intervals[i][1]:
                 return False
+        
         
         return True
