8c16ca333ccb42dc8b29e6279a6e9d580be993f5
Miao Jiang <miaojiang1987@gmail.com>
2019-11-27 14:54:10 -0500

    Review 146

--- Python/LRUCache.py
+++ Python/LRUCache.py
@@ -1,53 +1,66 @@
-class DoubleLinkedNode:
+class DoubLinkedList():
     def __init__(self):
-        self.key = None
-        self.value = None
-        self.prev = None
-        self.next = None
-
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.head = DoubleLinkedNode()
-        self.tail = DoubleLinkedNode()
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.map = {}
-        self.capacity = capacity
+        self.val=0
+        self.prev=None
+        self.next=None
+        self.key=None
         
-    def remove(self, node):    # head -> n2 -> n3 ->
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        
-    def add2head(self, node):
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.head=DoubLinkedList()
+        self.tail=DoubLinkedList()
+        self.hashmap={}
+        self.head.next=self.tail
+        self.tail.prev=self.head
+        self.capacity=capacity
     
-    def get(self, key: int) -> int:
-        if key in self.map:
-            node = self.map[key]
+    def remove(self,node):
+        node.prev.next=node.next
+        node.next.prev=node.prev
+        
+
+    def add2head(self,node):
+        node.next=self.head.next
+        node.prev=self.head
+        node.next.prev=node
+        self.head.next=node
+        
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        if key in self.hashmap:
+            node=self.hashmap[key]
             self.remove(node)
             self.add2head(node)
-            return node.value
+            return node.val
         else:
             return -1
 
-    def put(self, key: int, value: int) -> None:
-        if key not in self.map:
-            new_node = DoubleLinkedNode()
-            new_node.key = key
-            new_node.value = value
-            if len(self.map) >= self.capacity:
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: None
+        """
+        if key not in self.hashmap:
+            new_node=DoubLinkedList()
+            new_node.key=key
+            new_node.val=value
+            if len(self.hashmap) >= self.capacity:
                 last = self.tail.prev
                 self.remove(last)
-                del self.map[last.key]
-            self.map[key] = new_node
+                del self.hashmap[last.key]
             self.add2head(new_node)
-        
-        if key in self.map:
-            node = self.map[key]
-            node.value = value
+            self.hashmap[key]=new_node
+        else:
+            node = self.hashmap[key]
+            node.val = value
             self.remove(node)
             self.add2head(node)
