05334563781f61ac923ee919729fedeea349b3de
Miao Jiang <miaojiang1987@gmail.com>
2019-12-10 00:57:27 -0500

    an interview problem

---
+++ Python/karat_Meeting_room_1.py
@@ -0,0 +1,25 @@
+def meeting_room1(meetings, new_meeting):
+    meetings.sort()
+    for i in range(len(meetings)):
+        if i == 0 and new_meeting[1] <= meetings[i][1]:
+            return True
+        if new_meeting[0] >= meetings[i-1][1] or new_meeting[1] <= meetings[i][0]:
+            return True
+        if i == len(meetings) - 1 and new_meeting[0] >= meetings[i][1]:
+            return True
+            
+    return False
+
+def meeting_room_overlap(meetings, new_meeting):
+    meetings.sort()
+    available = False
+    for i in range(len(meetings)):   
+        if i == 0 and new_meeting[1] <= meetings[i][1]:
+            available = True
+        if new_meeting[0] >= meetings[i-1][1] and new_meeting[1] <= meetings[i][0]:
+            available = True
+        if i == len(meetings) - 1 and new_meeting[0] >= meetings[i][1]:
+            available = True           
+        if available == True:
+            return False
+    return True
